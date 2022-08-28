f7708d8749c88525f8ca16fda8f908a8266d2e38
Admin <Admin@LAPTOP-4P4HLUTQ>
2022-08-28 22:55:51 +0700

    Add Order service

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "LuanNguyen", FirstName = "Luan", LastName = "Nguyen", EmailAddress = "luannguyen@gmail.com", AddressLine = "HoChiMinh", Country = "VietNam", TotalPrice = 350
+                , ZipCode="ZipCode1232435",State="Demo State", CardName="LuanCard", CardNumber="34534654",Expiration="demo Expiration"
+                , CVV = "342", PaymentMethod = 1
+                ,CreatedBy = "LuanNguyen",CreatedDate = DateTime.Now,LastModifiedBy = "LuanNguyen"}
+            };
+        }
+    }
+}
