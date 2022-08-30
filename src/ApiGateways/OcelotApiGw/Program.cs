998ea0494ab2bf447dbc2a0681fa9d2998f1980f
Admin <Admin@LAPTOP-4P4HLUTQ>
2022-08-30 23:39:35 +0700

    Add config api gate way json

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,6 +1,30 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add configuration providers
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.Services.AddOcelot();//.AddCacheManager(settings => settings.WithDictionaryHandle());
+
+// Configure logging to the console.
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
